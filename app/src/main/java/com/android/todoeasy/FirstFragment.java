df5a9772d8e9f10f7115c872efd0b9442d9d8b4d
Yazan <yazan.kassoua@gmail.com>
2020-09-29 23:49:28 +0200

    Add todo expandable listView
       - Add a hard coded todo for testing.
       - Group view contains: todo name, expiryDate and finish button.
       - On group view click the child view expands and show the todo description.
    
    tde-01-todo-list

--- app/src/main/java/com/android/todoeasy/FirstFragment.java
+++ app/src/main/java/com/android/todoeasy/FirstFragment.java
@@ -4,18 +4,29 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.BaseExpandableListAdapter;
+import android.widget.ExpandableListView;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
-import androidx.navigation.fragment.NavHostFragment;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
 public class FirstFragment extends Fragment {
+
+    private List<Todo> todos;
+    private BaseExpandableListAdapter listAdapter;
+    private ExpandableListView todoListView;
 
     @Override
     public View onCreateView(
             LayoutInflater inflater, ViewGroup container,
             Bundle savedInstanceState
     ) {
+        super.onCreateView(inflater, container, savedInstanceState);
+        todos = new ArrayList<>();
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_first, container, false);
     }
@@ -23,12 +34,18 @@ public class FirstFragment extends Fragment {
     public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
-        view.findViewById(R.id.button_first).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                NavHostFragment.findNavController(FirstFragment.this)
-                        .navigate(R.id.action_FirstFragment_to_SecondFragment);
-            }
-        });
+        Todo newtodo = Todo.builder()
+                .name("Yesssssssss")
+                .expiryDate(LocalDateTime.now())
+                .finished(false)
+                .description("this is an initial todo")
+                .build();
+
+        todos.add(newtodo);
+
+        todoListView = getView().findViewById(R.id.todoListView);
+        listAdapter = new TodoListAdapter(this.getContext(), this.todos);
+        todoListView.setAdapter(listAdapter);
     }
+
 }
---
+++ app/src/main/java/com/android/todoeasy/TodoListAdapter.java
@@ -0,0 +1,92 @@
+package com.android.todoeasy;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseExpandableListAdapter;
+import android.widget.Button;
+import android.widget.TextView;
+
+import java.util.List;
+
+public class TodoListAdapter extends BaseExpandableListAdapter {
+
+    private Context context;
+    private List<Todo> todos;
+
+    public TodoListAdapter(Context context, List<Todo> todos) {
+        this.context = context;
+        this.todos = todos;
+    }
+
+    @Override
+    public View getGroupView(int groupPosition, boolean isExpanded, View convertView, ViewGroup parent) {
+        if (convertView == null) {
+            LayoutInflater inflater = (LayoutInflater) this.context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            convertView = inflater.inflate(R.layout.todo_list_item_group, null);
+        }
+        Todo todo = todos.get(groupPosition);
+
+        TextView todoName = convertView.findViewById(R.id.todoName);
+        todoName.setText(todo.getName());
+        TextView todoExpiryDate = convertView.findViewById(R.id.todoExpiryDate);
+        todoExpiryDate.setText(todo.getExpiryDate().toString());
+        Button finishButton = convertView.findViewById(R.id.finishButton);
+        finishButton.setVisibility(todo.isFinished() ? View.VISIBLE : View.GONE);
+        return convertView;
+    }
+
+    @Override
+    public View getChildView(int groupPosition, int childPosition, boolean isLastChild, View convertView, ViewGroup parent) {
+        if (convertView == null) {
+            LayoutInflater inflater = (LayoutInflater) this.context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            convertView = inflater.inflate(R.layout.todo_list_item_child, null);
+        }
+        Todo todo = todos.get(groupPosition);
+
+        TextView todoDescription = convertView.findViewById(R.id.todoDescription);
+        todoDescription.setText(todo.getDescription());
+        return convertView;
+    }
+
+    @Override
+    public int getGroupCount() {
+        return todos.size();
+    }
+
+    @Override
+    public int getChildrenCount(int groupPosition) {
+        return 1;
+    }
+
+    @Override
+    public Object getGroup(int groupPosition) {
+        return this.todos.get(groupPosition);
+    }
+
+    @Override
+    public Object getChild(int groupPosition, int childPosition) {
+        return this.todos.get(groupPosition);
+    }
+
+    @Override
+    public long getGroupId(int groupPosition) {
+        return groupPosition;
+    }
+
+    @Override
+    public long getChildId(int groupPosition, int childPosition) {
+        return childPosition;
+    }
+
+    @Override
+    public boolean hasStableIds() {
+        return false;
+    }
+
+    @Override
+    public boolean isChildSelectable(int groupPosition, int childPosition) {
+        return false;
+    }
+}
